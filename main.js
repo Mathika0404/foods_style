b54cdb8f5c2b5e9bb67ee3249e2f2a981382804f
Mathika0404 <mathikaselvaraj0404@gmail.com>
2025-12-02 17:56:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+// Onscroll Function in nav bar
+
+const navBar = document.querySelector('header');
+
+window.onscroll = () => {
+  navBar.classList.toggle('active', window.scrollY > 100);
+}
+
+
+function toggleMenu() {
+  document.querySelector(".navbar").classList.toggle("active");
+  document.querySelector(".menu-toggle").classList.toggle("active");
+}
+
+document.querySelectorAll(".arrow").forEach((arrow) => {
+  arrow.addEventListener("click", function (e) {
+    e.stopPropagation();
+    this.parentElement.classList.toggle("active");
+  });
+});
+
+document.querySelectorAll(".arrow, .nav-item > a").forEach((elem) => {
+  elem.addEventListener("click", function (e) {
+    e.preventDefault(); // prevent navigation if needed
+    e.stopPropagation();
+    this.closest(".nav-item").classList.toggle("active");
+  });
+});
+
+
+
+
+// Swiper JavaScript
+
+const swiper = new Swiper('.slider-wrapper', {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
+
+
+// ==Scroll Reveal===
+
+ScrollReveal({
+  reset:true,
+  distance: '100px',
+  duration: 2500
+})
+
+ScrollReveal().reveal('.home-text-content, .heading-text, .about-text, .product-description, .context-text', {origin : 'top'});
+ScrollReveal().reveal('.about-img, .home-text-content h1, .price, .small, .order-img,  .contact-img', { origin : 'left' });
+ScrollReveal().reveal('.cart-icon, .product-box h2, .order-text, .home-img, .contact-input, .heading-text p, .contact-text', { origin : 'right' });
+ScrollReveal().reveal('.button,  .menu-next, .footer-content, .product-box, .gallary-box', { origin : 'bottom' });
